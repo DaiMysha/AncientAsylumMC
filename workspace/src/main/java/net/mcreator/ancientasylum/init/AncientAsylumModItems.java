5ea005c654baa39c7123ddbbe81485da175e312f
Rylen <daimysha@gmail.com>
2024-12-26 19:05:10 +0100

    Push original workspace

---
+++ workspace/src/main/java/net/mcreator/ancientasylum/init/AncientAsylumModItems.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.ancientasylum.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.ancientasylum.AncientAsylumMod;
+
+public class AncientAsylumModItems {
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(AncientAsylumMod.MODID);
+	public static final DeferredItem<Item> RUBBER = REGISTRY.register("rubber", RubberItem::new);
+	// Start of user code block custom items
+	// End of user code block custom items
+}
